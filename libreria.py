0419c7453bed6846a34cfd2e139f2750fe7ad93a
GrethellR <58376183+grethellr@users.noreply.github.com>
2020-01-17 14:57:29 -0500

    Ejercicio 2

--- libreria.py
+++ libreria.py
@@ -8,3 +8,16 @@ def valida_Dni(strDNI):
     else:
         return True
 #fin_valida_Dni
+
+#Ejercicio 2
+
+def valida_Vocal(strVocal):
+    #1.strVocal es un caracter
+    #2.Se validara si es vocal
+    if(strVocal=='a' or strVocal== 'e' or strVocal=='i' or strVocal=='o' or strVocal=='u' ):
+        return True
+    else:
+        return False
+#fin_valida_Vocal
+
+
--- test.py
+++ test.py
@@ -6,3 +6,12 @@ assert (libreria.valida_Dni("123456789")== False)
 assert (libreria.valida_Dni("123456")== False)
 assert (libreria.valida_Dni("12345678")== True)
 print("valida_Dni OK")
+
+assert (libreria.valida_Vocal("a")== True)
+assert (libreria.valida_Vocal("r")== False)
+assert (libreria.valida_Vocal("i")== True)
+assert (libreria.valida_Vocal("u")== True)
+assert (libreria.valida_Vocal("f")== False)
+print("valida_Vocal OK")
+
+
